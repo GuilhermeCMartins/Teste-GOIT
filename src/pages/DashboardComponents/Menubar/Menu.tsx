08f08ef9dc17485a9ed521547f4f88b27560e55b
Guilherme Martins <guil54@hotmail.com>
2023-04-26 02:13:00 -0300

    Arrumando svgs

--- src/pages/DashboardComponents/Menubar/Menu.tsx
+++ src/pages/DashboardComponents/Menubar/Menu.tsx
@@ -18,11 +18,11 @@ function Menu(){
                 <li className={router.pathname === '/painel' ? styles.active : styles.inactive}><Image src="/images/Dashboard/Paineldecontrole.svg" width={24} height={24} alt='Logo'></Image><Link href='/painel'>Painel de Controle</Link></li>
                 <li className={router.pathname === '/dashboard' ? styles.active : styles.inactive}><Image src="/images/Dashboard/perfil.svg" width={24} height={24} alt='Logo'></Image><Link href='/dashboard'>Perfil</Link></li>
                 <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/Segurança.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>Segurança</Link></li>
-                <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/Carteira.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>Carteira</Link></li>
-                <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/Cadastro.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>Cadastro complementar</Link></li>
+                <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/carteira.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>Carteira</Link></li>
+                <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/cadastro.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>Cadastro complementar</Link></li>
                 <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/mkt.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>MKT Place Personalizado</Link></li>
                 <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/config.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>Configurações</Link></li>
-                <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/Integração.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>Sair</Link></li>
+                <li className={router.pathname === '/' ? styles.active : styles.inactive}><Image src="/images/Dashboard/integração.svg" width={24} height={24} alt='Logo'></Image><Link href='/'>Sair</Link></li>
             </ul>
         </div>
     </>
